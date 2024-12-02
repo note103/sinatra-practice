450356c1301a1a6ac8c279be70ff4992ebaf9fe7
Hiroaki Kadomatsu <note103@gmail.com>
2024-12-03 00:16:19 +0900

    exec_paramsをプリペアドステートメントに置き換え

--- models/article_repository.rb
+++ models/article_repository.rb
@@ -5,41 +5,39 @@ require 'pg'
 class ArticleRepository
   DB_NAME = 'articles_db'
 
+  def self.connection
+    @connection ||= PG.connect(dbname: DB_NAME)
+  end
+
+  def self.prepare_statements
+    connection.prepare('select_all', 'SELECT * FROM articles ORDER BY created_at ASC')
+    connection.prepare('select_by_id', 'SELECT * FROM articles WHERE id = $1 LIMIT 1')
+    connection.prepare('insert_article', 'INSERT INTO articles (title, body, created_at) VALUES ($1, $2, NOW())')
+    connection.prepare('update_article', 'UPDATE articles SET title = $1, body = $2 WHERE id = $3')
+    connection.prepare('delete_article', 'DELETE FROM articles WHERE id = $1')
+  end
+
   def self.all
-    connection = PG.connect(dbname: DB_NAME)
-    result = connection.exec('SELECT * FROM articles ORDER BY created_at ASC')
-    connection.close
+    result = connection.exec_prepared('select_all')
     result.map { |row| row }
   end
 
   def self.find(id)
-    connection = PG.connect(dbname: DB_NAME)
-    result = connection.exec_params('SELECT * FROM articles WHERE id = $1 LIMIT 1', [id])
-    connection.close
+    result = connection.exec_prepared('select_by_id', [id])
     result.ntuples == 1 ? result[0] : nil
   end
 
   def self.add(article)
-    connection = PG.connect(dbname: DB_NAME)
-    connection.exec_params(
-      'INSERT INTO articles (title, body, created_at) VALUES ($1, $2, NOW())',
-      [article['title'], article['body'] || '']
-    )
-    connection.close
+    connection.exec_prepared('insert_article', [article['title'], article['body'] || ''])
   end
 
   def self.update(id, attributes)
-    connection = PG.connect(dbname: DB_NAME)
-    connection.exec_params(
-      'UPDATE articles SET title = $1, body = $2 WHERE id = $3',
-      [attributes['title'], attributes['body'] || '', id]
-    )
-    connection.close
+    connection.exec_prepared('update_article', [attributes['title'], attributes['body'] || '', id])
   end
 
   def self.delete(id)
-    connection = PG.connect(dbname: DB_NAME)
-    connection.exec_params('DELETE FROM articles WHERE id = $1', [id])
-    connection.close
+    connection.exec_prepared('delete_article', [id])
   end
 end
+
+ArticleRepository.prepare_statements
